1404935371e9d71613a8e84c4c29fa937b5b1d72
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-15 14:23:23 +0900

    Building Another Wrapper Component (Custom Form Component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useRef } from "react";
+import Button from "./components/Button.tsx";
+import Form from "./components/Form.tsx";
 import Input from "./components/Input.tsx";
 
 function App() {
-	const input = useRef<HTMLInputElement>(null);
-
 	return (
 		<main>
-			<Input label="Test" id="test" ref={input} />
+			<Form>
+				<Input type="text" label="Name" id="name" />
+				<Input type="number" label="Age" id="age" />
+				<p>
+					<Button>Save</Button>
+				</p>
+			</Form>
 		</main>
 	);
 }
