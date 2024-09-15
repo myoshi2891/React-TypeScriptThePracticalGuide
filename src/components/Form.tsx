6ec380d76276d53c040f4be890c51a71f1df5c35
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-15 15:10:57 +0900

    Exposing Component APIs with useImperativeHandle (with TypeScript)

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,49 @@
-import { ComponentPropsWithoutRef, FormEvent } from "react";
+import {
+	ComponentPropsWithoutRef,
+	FormEvent,
+	forwardRef,
+	useImperativeHandle,
+	useRef,
+} from "react";
+
+export type FormHandle = {
+	clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
 	onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+	{ onSave, children, ...otherProps },
+	ref
+) {
+	const form = useRef<HTMLFormElement>(null);
+
+	useImperativeHandle(ref, () => {
+		return {
+			clear() {
+				console.log("clearing");
+				form.current?.reset();
+			},
+		};
+	});
+
 	function handleSubmit(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault();
 
 		const formData = new FormData(event.currentTarget);
 		const data = Object.fromEntries(formData);
 		onSave(data);
+
+		// form.current?.reset();
 	}
 
 	return (
-		<form onSubmit={handleSubmit} {...otherProps}>
+		<form onSubmit={handleSubmit} {...otherProps} ref={form}>
 			{children}
 		</form>
 	);
-}
+});
+
+export default Form;
