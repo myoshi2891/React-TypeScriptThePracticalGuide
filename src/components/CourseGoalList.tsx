1c5d784edfff308df62ad0aca49946043bb9dc6d
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-15 12:03:42 +0900

    Building a More Dynamic & Flexible Component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,24 +1,52 @@
-import CourseGoal from './CourseGoal.tsx';
-import { type CourseGoal as CGoal } from '../App.tsx';
+import { type ReactNode } from "react";
+import { type CourseGoal as CGoal } from "../App.tsx";
+import CourseGoal from "./CourseGoal.tsx";
+import InfoBox from "./InfoBox.tsx";
 
 type CourseGoalListProps = {
-  goals: CGoal[];
-  onDeleteGoal: (id: number) => void;
+	goals: CGoal[];
+	onDeleteGoal: (id: number) => void;
 };
 
 export default function CourseGoalList({
-  goals,
-  onDeleteGoal,
+	goals,
+	onDeleteGoal,
 }: CourseGoalListProps) {
-  return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
-  );
+	if (goals.length === 0) {
+		return (
+			<InfoBox mode="hint">
+				You have no course goals yet. Start adding some!
+			</InfoBox>
+		);
+	}
+
+	let warningBox: ReactNode;
+
+	if (goals.length >= 4) {
+		warningBox = (
+			<InfoBox mode="warning">
+				You're collecting a lot of goals. Don't put too much on your
+				plate!
+			</InfoBox>
+		);
+	}
+
+	return (
+		<>
+			{warningBox}
+			<ul>
+				{goals.map((goal) => (
+					<li key={goal.id}>
+						<CourseGoal
+							id={goal.id}
+							title={goal.title}
+							onDelete={onDeleteGoal}
+						>
+							<p>{goal.description}</p>
+						</CourseGoal>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
