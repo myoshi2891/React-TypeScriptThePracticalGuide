a48bf5592943ed87f07a5cb9dfe0009a05dd346f
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-16 16:20:52 +0900

    Setting a State Type

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,6 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type CartIrem = {
+	id: string;
+	title: string;
+	price: number;
+	quantity: number;
+};
+
+type CartState = {
+	items: CartItem[];
+};
+
+const initialState: CartState = {
+	items: [],
+};
 export const cartSlice = createSlice({
 	name: "cart",
 	initialState,
+	reducers: {
+		addToCart() {},
+		removeFromCart() {},
+	},
 });
